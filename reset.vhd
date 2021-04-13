78de53e4b7b91a44cdd343527065e5d14e64d671
Fouad Smaoui <77508987+Fouad-Smaoui@users.noreply.github.com>
2021-04-13 03:05:35 +0200

    Update reset.vhd

--- reset.vhd
+++ reset.vhd
@@ -1,14 +1,21 @@
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
+USE ieee.std_logic_1164.ALL;
 
-ENTITY reset_module IS
-  PORT(
+	ENTITY reset_module IS
+	-- Generic ( M= )   -- uncomment the generic section and declare a value 
+  	PORT(
 		reset    : IN  STD_LOGIC; 
-		counter  : OUT STD_LOGIC);
-END reset_module;
-ARCHITECTURE arch_reset_module OF reset_module IS
+		counter  : OUT STD_LOGIC_VECTOR(M downto 0)        -- assign the generic value to the counter size 
+	    );
+		
+	END reset_module;
 
-				If(reset = '1') THEN                 					--reset counter because input is changing
-                counter <= (OTHERS => '0');
- 
-  END arch_reset_module;
+	ARCHITECTURE arch_reset_module OF reset_module IS
+
+	 	If(reset = '1') THEN             --reset counter because input is changing
+                	
+			counter <= (OTHERS => '0');   
+		
+ 		end if;	
+			
+  	END arch_reset_module;
